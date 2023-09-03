919e19b952f2c7f9f9e9261e61aaa06081ffee48
qzonic <lazzytoo@mail.ru>
2023-09-03 18:58:05 +0400

    Create docs

--- todo/api/urls.py
+++ todo/api/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include, path
+from django.views.generic import TemplateView
 from rest_framework.routers import DefaultRouter
 
 from .views import CategoryViewSet, TaskViewSet
@@ -15,6 +16,11 @@ urlpatterns = [
     path('', include(router.urls)),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
+    path(
+        'redoc/',
+        TemplateView.as_view(template_name='redoc.html'),
+        name='redoc'
+    ),
 ]
 
 if settings.DEBUG:
